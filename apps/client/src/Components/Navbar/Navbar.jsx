50d7522a1e1597676fa1bcdf64749f428865d23c
enclar <107608010+enclar@users.noreply.github.com>
2022-10-25 21:49:22 +0800

    added in react routing

--- apps/client/src/Components/Navbar/Navbar.jsx
+++ apps/client/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Outlet, Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div id="navbar">
+      <div id="tabs">
+        <Link to="/jobs">Find Jobs</Link>
+        <Link to="/jobs/create">Create Job</Link>
+        <Link to="/user">My Profile</Link>
+      </div>
+      <Outlet />
+    </div>
   )
 }
 
