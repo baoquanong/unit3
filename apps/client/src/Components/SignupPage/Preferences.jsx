fe462083016242de27fedbc7deec682b4819dfd4
enclar <107608010+enclar@users.noreply.github.com>
2022-10-28 21:52:42 +0800

    client: created a form for users to specify their preferences/details about themselves

--- apps/client/src/Components/SignupPage/Preferences.jsx
+++ apps/client/src/Components/SignupPage/Preferences.jsx
@@ -1,7 +1,53 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { DataContext } from "../../App";
+
 const Preferences = () => {
+    // setting up context
+    const { state, setState } = useContext(DataContext);
+
     return (
         <div id="preferences">
-            Preferences
+            <form autoComplete="off">
+                <h1>PREFERENCES</h1>
+                <p>
+                    Tell us more about yourself and what you're looking for! <br />
+                    Your preferences can always be updated later in your profile
+                </p>
+                <section id="interests">
+                    <legend>I'm interested in helping/looking for help in these areas:</legend>
+                    <label>
+                        <input type="checkbox" name="handywork" />
+                        Handywork
+                    </label>
+                    <label>
+                        <input type="checkbox" name="cleaning" />
+                        Cleaning
+                    </label>
+                    <label>
+                        <input type="checkbox" name="caregiving" />
+                        Caregiving
+                    </label>
+                    <label>
+                        <input type="checkbox" name="pets" />
+                        Pets
+                    </label>
+                    <label>
+                        <input type="checkbox" name="events" />
+                        Events
+                    </label>
+                    <label>
+                        <input type="checkbox" name="education" />
+                        Education
+                    </label>
+                </section>
+                <label>
+                    Here's a little bit about myself:
+                    <textarea type="text" name="description" />
+                </label>
+                <button>Create My Account</button>
+            </form>
         </div>
     );
 };
