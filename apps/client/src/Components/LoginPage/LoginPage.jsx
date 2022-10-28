2db40b9d5eef074e8d81914aaeaba0ed2ee1d914
enclar <107608010+enclar@users.noreply.github.com>
2022-10-28 16:06:02 +0800

    client: added in form for existing users to log into website

--- apps/client/src/Components/LoginPage/LoginPage.jsx
+++ apps/client/src/Components/LoginPage/LoginPage.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 function LoginPage() {
+  // setting up state
+  const [loginDetails, setLoginDetails] = useState({
+    username: "",
+    password: "",
+  });
+
+  // function to handle onChange
+  const handleChange = (event, field) => {
+    console.log(event.target.value);
+    setLoginDetails({...loginDetails, [`${field}`]: event.target.value});
+  };
+
   return (
-    <div id="login-page">LoginPage</div>
-  )
-}
+    <div id="login-page">
+      <h1>LOGIN</h1>
+      <form autoComplete="off">
+        <div id="inputs">
+          <label>
+            Username:
+            <input
+              type="text"
+              value={loginDetails.username}
+              onChange={() => handleChange(event, "username")}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              value={loginDetails.password}
+              onChange={() => handleChange(event, "password")}
+            />
+          </label>
+        </div>
+        <button>Let's Go!</button>
+      </form>
+    </div>
+  );
+};
 
 export default LoginPage;
