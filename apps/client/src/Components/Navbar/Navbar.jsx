3fde4ff5f2624c8c298bc5e07172172e88043788
enclar <107608010+enclar@users.noreply.github.com>
2022-10-28 15:33:36 +0800

    client: added styling for navbar

--- apps/client/src/Components/Navbar/Navbar.jsx
+++ apps/client/src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React from "react";
 import { Outlet, Link } from "react-router-dom";
+
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <div id="navbar">
-      <div id="tabs">
-        <Link to="/jobs">Find Jobs</Link>
-        <Link to="/jobs/create">Create Job</Link>
-        <Link to="/user">My Profile</Link>
+    <div id="layout">
+      <div id="navbar">
+        <h1><span>SIDE</span> HUSTLE</h1>
+        <div id="tabs">
+          <Link to="/jobs">Find Jobs</Link>
+          <Link to="/jobs/create">Create Job</Link>
+          <Link to="/user">My Profile</Link>
+        </div>
       </div>
       <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
