577dd2277e9625782fe5c8ebba4f0193d28d250b
enclar <107608010+enclar@users.noreply.github.com>
2022-10-31 23:44:28 +0800

    client: refactored code to map out user reviews and revised styling

--- apps/client/src/Components/Profile/Profile.jsx
+++ apps/client/src/Components/Profile/Profile.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 
 import Reviews from "./Reviews";
 import { DataContext } from '../../App';
+import reviews from "./reviews";
 import "./Profile.css";
 
 function Profile() {
@@ -9,11 +10,15 @@ function Profile() {
   const { state, setState } = useContext(DataContext);
   const user = state.currUserInfo;
 
+  const totalRating = reviews.reduce((total, num) => total + num.stars, 0);
+  const avgRating = totalRating/reviews.length;
+
   return (
     <div id="profile">
       <div id="user-info">
         <img src={user.img} />
         <h2>{user.username.toUpperCase()}</h2>
+        <p id="rating">Average Rating: {avgRating}/5</p>
         <div id="about-me">
           <p className="header">ABOUT ME</p>
           <p>{user.aboutMe}</p>
--- apps/client/src/Components/Profile/Reviews.jsx
+++ apps/client/src/Components/Profile/Reviews.jsx
@@ -1,40 +1,34 @@
 import { useContext } from "react";
+
 import { DataContext } from "../../App";
+import reviews from "./reviews";
 
 const Reviews = () => {
     // setting up context
     const { state, setState } = useContext(DataContext);
     const user = state.currUserInfo;
 
+    // mapping out user reveiws
+    const userReviews = reviews.map((review, index) => {
+        return (
+            <div className="review" key={index}>
+                <div>
+                    <h4>{review.postedBy}</h4>
+                    <p className="hired">
+                        {review.type === "hired" ? "Hired Sally" : "Hired by Sally"}
+                    </p>
+                </div>
+                <p className="rating">Rating: {review.stars}/5</p>
+                <p>{review.review}</p>
+            </div>
+        );
+    });
+
     return (
         <div id="user-reviews">
             <h3>Here's what other users had to say about you!</h3>
             <div id="reviews">
-                <div className="review">
-                    <h4>Derek W.</h4>
-                    <p className="hired">Hired Sally</p>
-                    <p>Sally did a great job of checking in and sending food to my Mother when I was overseas. She even helped her to fix her TV remote. Would hire Sally again!</p>
-                </div>
-                <div className="review">
-                    <h4>Aaron H.</h4>
-                    <p className="hired">Hired Sally</p>
-                    <p>Did a decent job, although not very familiar with more complicated repairs/work (as stated in her profile).</p>
-                </div>
-                <div className="review">
-                    <h4>Mary T.</h4>
-                    <p className="hired">Hired by Sally</p>
-                    <p>Sally was responsive and gave clear instructions on what she needed me to do. Would work for her again!</p>
-                </div>
-                <div className="review">
-                    <h4>Rachel L.</h4>
-                    <p className="hired">Hired by Sally</p>
-                    <p>Had a slight miscommunication but Sally was nice and we managed to resolve it properly.</p>
-                </div>
-                <div className="review">
-                <h4>Megan N.</h4>
-                    <p className="hired">Hired Sally</p>
-                    <p>Told me she had experience with electrical rewiring but wasn't able to complete the job. 2 stars for service recovery as she recommended a replacement who turned out to be great and affordably priced.</p>
-                </div>
+                {userReviews}
             </div>
         </div>
     );
