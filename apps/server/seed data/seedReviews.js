51f313878155fe3bb6913e00e3d0690b3d7d1e71
enclar <107608010+enclar@users.noreply.github.com>
2022-11-01 21:37:48 +0800

    server: added post route for users to login (testing tbc)

---
+++ apps/server/seed data/seedReviews.js
@@ -0,0 +1,28 @@
+const seedUsers = [
+    {
+        postedBy: "63611626fcafb2c48bef34a9",
+        postedFor: "63611626fcafb2c48bef34a4",
+        message: "Wendy did an exellent job! Would totally recommend her",
+        rating: 5
+    },
+    {
+        postedBy: "63611626fcafb2c48bef34a9",
+        postedFor: "63611626fcafb2c48bef34a6",
+        message: "Okay. Not bad!",
+        rating: 3
+    },
+    {
+        postedBy: "63611626fcafb2c48bef34aa",
+        postedFor: "63611626fcafb2c48bef34a7",
+        message: "Natalie was overly friendly, didn't sit well with us",
+        rating: 2
+    },
+    {
+        postedBy: "63611626fcafb2c48bef34b1",
+        postedFor: "63611626fcafb2c48bef34ab",
+        message: "Great help! Would find Ben again",
+        rating: 4
+    },
+];
+
+module.exports = seedReviews;
