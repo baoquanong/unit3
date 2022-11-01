f4d5965adae50470458de72e8d5c03a0e446370f
enclar <107608010+enclar@users.noreply.github.com>
2022-11-01 20:09:24 +0800

    server: added in seed data and route for users

---
+++ apps/server/seed data/seedUsers.js
@@ -0,0 +1,25 @@
+const seedUsers = [
+    {
+        username: "Sally",
+        password: "pass1234",
+        email: "sally@mail.com",
+        skills: ["Handywork", "Elderly"],
+        description: "Live with elderly grandparents and take care of them",
+    },
+    {
+        username: "Mary",
+        password: "hello",
+        email: "mary@mail.com",
+        skills: ["Handywork", "Cleaning"],
+        description: "Able to do general housework and minor fixing",
+    },
+    {
+        username: "Mary",
+        password: "hello",
+        email: "mary@mail.com",
+        skills: ["Handywork", "Cleaning"],
+        description: "Able to do general housework and minor fixing",
+    }
+];
+
+module.exports = seedUsers;
