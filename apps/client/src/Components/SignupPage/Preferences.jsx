114d965f2fef69d76278606af1efcbe1e6f66fcf
enclar <107608010+enclar@users.noreply.github.com>
2022-11-03 14:46:08 +0800

    client: processed form inputs in preferences component to match the format required by schema

--- apps/client/src/Components/SignupPage/Preferences.jsx
+++ apps/client/src/Components/SignupPage/Preferences.jsx
@@ -7,39 +7,58 @@ const Preferences = () => {
     // setting up context
     const { state, setState } = useContext(DataContext);
 
+    // function to handle onSubmit
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // formatting data from form to fit format required by schema
+        const userData = Object.fromEntries(new FormData(event.target));
+        const userSkills = Object.keys(userData);
+        userSkills.pop();
+
+        const userPrefs = {
+            skills: userSkills,
+            description: userData.description
+        };
+    };
+
     return (
         <div id="preferences">
-            <form id="preferences-form" autoComplete="off">
+            <form
+                id="preferences-form"
+                autoComplete="off"
+                onSubmit={handleSubmit}
+            >
                 <h1>PREFERENCES</h1>
                 <p>
                     Tell us more about yourself and what you're looking for! <br />
                     Your preferences can always be updated later in your profile
                 </p>
                 <section id="interests">
-                    <legend>I'm interested in helping/looking for help in these areas:</legend>
+                    <legend>I'm interested in helping in these areas:</legend>
                     <div id="check-inputs">
                         <label>
-                            <input type="checkbox" name="handywork" />
+                            <input type="checkbox" name="Handywork" />
                             Handywork
                         </label>
                         <label>
-                            <input type="checkbox" name="cleaning" />
+                            <input type="checkbox" name="Cleaning" />
                             Cleaning
                         </label>
                         <label>
-                            <input type="checkbox" name="caregiving" />
+                            <input type="checkbox" name="Caregiving" />
                             Caregiving
                         </label>
                         <label>
-                            <input type="checkbox" name="pets" />
+                            <input type="checkbox" name="Pets" />
                             Pets
                         </label>
                         <label>
-                            <input type="checkbox" name="events" />
+                            <input type="checkbox" name="Events" />
                             Events
                         </label>
                         <label>
-                            <input type="checkbox" name="education" />
+                            <input type="checkbox" name="Education" />
                             Education
                         </label>
                     </div>
