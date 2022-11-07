d7c3b3b5198344e2cc019bf0b606386bb4911f71
enclar <107608010+enclar@users.noreply.github.com>
2022-11-07 12:23:27 +0800

    client: added in dropdown menu

--- apps/client/src/Components/Navbar/Navbar.jsx
+++ apps/client/src/Components/Navbar/Navbar.jsx
@@ -7,15 +7,31 @@ function Navbar() {
   // setting up navigation
   const navigate = useNavigate();
 
+  // function to show dropdown
+  const handleDropdown = () => {
+    document.getElementById("dropdown-content").classList.toggle("show");
+  };
+
   return (
     <div id="layout">
       <div id="navbar">
-        <h1 onClick={() => navigate("/")}><span>SIDE</span> HUSTLE</h1>
+        <h1 onClick={() => navigate("/")}><span>SIDE</span>HUSTLE</h1>
         <div id="tabs">
-          <Link to="/jobs">Find Jobs</Link>
-          <Link to="/jobs/create">Create Job</Link>
-          <Link to="/user">My Profile</Link>
-          <Link to="/">Logout</Link>
+          <Link className="tab" to="/jobs">FIND JOBS</Link>
+          <Link className="tab" to="/jobs/create">CREATE</Link>
+          <div id="dropdown">
+            <p
+              className="tab"
+              id="profile-tab"
+              onClick={handleDropdown}
+            >PROFILE</p>
+            <div className="dropdown-content" id="dropdown-content">
+              <Link to="/user">OVERVIEW</Link>
+              <Link to="/user/jobs">JOBS</Link>
+              <Link to="/user/reviews">REVIEWS</Link>
+              <Link to="/">LOGOUT</Link>
+            </div>
+          </div>
         </div>
       </div>
       <Outlet />
