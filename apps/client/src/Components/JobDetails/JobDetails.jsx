b6fd89e6fed23d1ce94a63263f5f80c9639e8dce
enclar <107608010+enclar@users.noreply.github.com>
2022-11-08 15:19:50 +0800

    client: added styling to job details page

--- apps/client/src/Components/JobDetails/JobDetails.jsx
+++ apps/client/src/Components/JobDetails/JobDetails.jsx
@@ -1,39 +1,34 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router";
+
 import { DataContext } from "../../App";
+import "./JobDetails.css";
 
 function JobDetails() {
+  // setting up context
   const { state, setState } = useContext(DataContext);
   const job = state.currViewedJob;
+
+  // setting up navigation
   const navigate = useNavigate();
 
   return (
-    <div>
-      By Posted By : {job.postedBy.username}
-      <br />
-      Description : {job.jobDescription}
-      <br />
-      Job Type : {job.jobType}
-      <br />
-      Status: {job.jobStatus}
-      <br />
-      Job Start : {job.jobStart.slice(0, 10)}
-      <br />
-      Job End : {job.jobEnd.slice(0, 10)}
-      <br />
-      Location : {job.jobLocation}
-      <br />
-      Price : {job.jobPrice}
-      <br />
-      <button
-        onClick={() => {
-          navigate("/jobs");
-        }}
-      >
-        {" "}
-        Back{" "}
-      </button>
-      <button> Apply </button>
+    <div id="job-details">
+      <div id="curr-job">
+        <h1>{job.jobTitle.toUpperCase()}</h1>
+        <p id="description"><span>JOB DESCRIPTION:</span> {job.jobDescription}</p>
+        <div id="job-info">
+          <p><span>POSTED BY:</span> {job.postedBy.username}</p>
+          <p><span>JOB TYPE:</span> {job.jobType}</p>
+          <p><span>DATE:</span> {job.jobStart.slice(0, 10)} to {job.jobEnd.slice(0, 10)}</p> 
+          <p><span>LOCATION:</span> {job.jobLocation ? job.jobLocation : "TBC"}</p>
+          <p><span>PRICE:</span> {job.jobPrice ? job.jobPrice : "TBC"}</p>
+        </div>
+        <div id="details-buttons">
+          <button onClick={() => navigate("/jobs")}>BACK</button>
+          <button> APPLY </button>
+        </div>
+      </div>
     </div>
   );
 }
